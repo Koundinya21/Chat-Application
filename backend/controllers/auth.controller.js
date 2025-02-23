55a93ebae42a4ccd9d33c16325474e81e7bb630b
koundinya21 <koundinyamiddela21@gmail.com>
2025-02-23 17:58:35 +0530

    Solution

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,15 @@
+export const login=async(req,res)=>{
+    try{
+        const {fullName,username,password,confirmPassowrd,gender}=req.body;
+        
+    }catch(e){
+
+    }
+}
+
+export const signup=(req,res)=>{
+    console.log("signupUser")
+}
+export const logout=(req,res)=>{
+    console.log("logoutUser")
+}
